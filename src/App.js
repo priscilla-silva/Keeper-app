c164c2a4694b1ab21e3e0c8cdb9688776ae7a811
Priscilla Silva <priscilla.sg91@gmail.com>
2022-02-02 15:40:43 +0100

    notes added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Notes from './components/Notes';
 import './App.css';
 
 function App() {
@@ -10,7 +11,7 @@ return (
     <div className="App">
         <Header />
 
-
+            <Notes />
 
         <Footer />
     </div>
---
+++ src/components/Notes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../App.css';
+
+function Notes() {
+
+    return(
+        <div className="note">
+            <h1>Hello world!</h1>
+            <p>This is my first note</p>
+
+        
+        </div>
+
+
+    );
+
+}
+
+export default Notes;
